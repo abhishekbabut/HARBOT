b56fa9863a59ecb2c9e3ce0757ec21391c378203
Abhishek Babu T <76171622+abhishekbabut@users.noreply.github.com>
2023-05-11 21:12:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+def config():
+    config = {     
+      "apiKey": "AIzaSyBBeg75pP2pimr2uhY7am4R_kCD_41Lou4",
+      "authDomain": "fir-iot-d86c8-default-rtdb",
+      "databaseURL": "https://fir-iot-d86c8-default-rtdb.firebaseio.com/",
+      "storageBucket": "HARBOT"
+    }
+    return config
