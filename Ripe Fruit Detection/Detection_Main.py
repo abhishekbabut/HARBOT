2955c76dc6d559c26fb19cd27b1b4bd9e05e3585
Abhishek Babu T <76171622+abhishekbabut@users.noreply.github.com>
2023-05-13 22:38:14 +0530

    Add files via upload

---
+++ Ripe Fruit Detection/Detection_Main.py
@@ -0,0 +1,22 @@
+from ut import *
+from Stepper import *
+from Circle_Color_Detection import *
+
+def harvest():
+    a=0
+    while a<=4800:
+        red=color()
+        if red==0:
+            TOP('clk',800)
+        if red==1:
+            getObjects()
+            time.sleep(2)
+        a=a+800
+        
+    if a>4800:
+        TOP('ant',4800)
+
+harvest()
+
+    
+    
